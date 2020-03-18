fb287a43c07a577976bfcc4c4684e124b8cf50d9
GregJohnsonSince1987 <gregjohnsonsince1987@gmail.com>
2020-03-17 20:33:12 -0500

    Greg's first commit

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
  
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
-mongoose.connect(url);
+// DYNAMICALLY CREATE A "MongoDB Database"
+// THE "Port Number" IS NOW REQUIRED IN THE "mongoose.connect()" FUNCTION
+mongoose.connect("mongodb://localhost:27017/yelpbg_slider", {useUnifiedTopology: true, useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -50,6 +51,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
    console.log("The YelpCamp Server Has Started!");
 });
